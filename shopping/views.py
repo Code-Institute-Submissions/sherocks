309e24e65d70d06734caf52da37cec93c5f0ac51
marlene32100 <katia.debon@hotmail.it>
2021-01-31 20:26:58 +0000

    Fixed JavaScript in shopping bag template

--- shopping/views.py
+++ shopping/views.py
@@ -4,11 +4,12 @@ from products.models import Product
 from django.contrib import messages
 
 
-# Create your views here.
+# View that renders the shopping bag page
 def view_bag(request):
     return render(request, "shopping/shopping-bag.html")
 
 
+# View that allows to add products to the shopping bag
 def add_to_bag(request, item_id):
     product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get("quantity"))
@@ -23,20 +24,20 @@ def add_to_bag(request, item_id):
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
                 messages.success(
-                    request, f'Updated {size.upper()}to {bag[item_id]["items_by_size"][size]}')
+                    request, f'Updated size {size.upper()} for {product.name} to {bag[item_id]["items_by_size"][size]} pieces')
             else:
                 bag[item_id]['items_by_size'][size] = quantity
                 messages.success(
-                    request, f'You just added {product.name}, {size.upper()}')
+                    request, f'You just added {product.name}, size {size.upper()}')
         else:
             bag[item_id] = {'items_by_size': {size: quantity}}
             messages.success(
-                request, f'You just added {product.name}, {size.upper()}')
+                request, f'You just added {product.name}, size {size.upper()}')
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
             messages.success(
-                request, f'Nice! You added more pieces of {product.name}')
+                request, f'Nice! You added more {product.name} to your shopping bag')
         else:
             bag[item_id] = quantity
             messages.success(
@@ -46,6 +47,7 @@ def add_to_bag(request, item_id):
     return redirect(redirect_url)
 
 
+# View that allows to update products in the shopping bag
 def update_bag(request, item_id):
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get("quantity"))
@@ -57,14 +59,12 @@ def update_bag(request, item_id):
     if size:
         if quantity > 0:
             bag[item_id]["items_by_size"][size] = quantity
-            messages.success(
-                    request, f'Updated "{product.name}" in size {size.upper()} to {bag[item_id]["items_by_size"][size]}')
+            messages.success(request, f'Updated "{product.name}" in size {size.upper()} to {bag[item_id]["items_by_size"][size]}')
         else:
             del bag[item_id]["items_by_size"][size]
             if not bag[item_id]["items_by_size"]:
                 bag.pop()
-                messages.success(
-                    request, f'Removed "{product.name}", {size.upper()}')
+            messages.success(request, f'Removed "{product.name}", {size.upper()}')
     else:
         if quantity > 0:
             bag[item_id] = quantity
@@ -78,6 +78,7 @@ def update_bag(request, item_id):
     return redirect(reverse('shopping'))
 
 
+# View that allows to remove products form the shopping bag
 def remove_item(request, item_id):
     product = Product.objects.get(pk=item_id)
     try:
@@ -91,7 +92,7 @@ def remove_item(request, item_id):
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
                 messages.success(
-                    request, f'Removed "{product.name}", {size.upper()} from your shopping bag')
+                    request, f'Removed "{product.name}", size {size.upper()} from your shopping bag')
         else:
             bag.pop(item_id)
             messages.success(
@@ -102,5 +103,5 @@ def remove_item(request, item_id):
 
     except Exception as e:
         messages.error(
-            request, f'Something went wrong removing "{product.name}"')
+            request, f'Something went wrong while removing item: {e}"')
         return HttpResponse(status=500)
