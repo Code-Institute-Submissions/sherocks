3df62945282887dbe3089b718f956cee1f8a338b
marlene32100 <katia.debon@hotmail.it>
2021-02-04 12:05:34 +0000

    istalled django countries and fixed code in models and forms

--- checkout/models.py
+++ checkout/models.py
@@ -3,6 +3,7 @@ import uuid
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
+from django_countries.fields import CountryField
 
 from products.models import Product
 
@@ -16,7 +17,7 @@ class Order(models.Model):
         default="", max_length=100, null=True, blank=True)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label="Country *", null=False, blank=False)
     postcode = models.CharField(
         default="", max_length=20, null=False, blank=False)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
