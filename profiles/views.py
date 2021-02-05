aec01d6eeac0db01717f33aebbe7254dbf995ce3
marlene32100 <katia.debon@hotmail.it>
2021-02-05 21:30:42 +0000

    Added template to profile app

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
+    profile = get_object_or_404(UserProfile, user=request.user)
     template = "profiles/profile.html"
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
     context = {
-
+        "orders": orders,
+        "form": form,
     }
     return render(request, template, context)
