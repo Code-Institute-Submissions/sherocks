b43051837e48fed27fd851dcc5238ff96d9158d3
marlene32100 <katia.debon@hotmail.it>
2021-02-09 08:39:38 +0000

    Added edit product functionality

--- products/views.py
+++ products/views.py
@@ -109,3 +109,27 @@ def add_product(request):
         "form": form,
     }
     return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Great! Product updated.')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(
+                request, 'Failed to update product. Check your form.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'Now editing {product.name}')
+
+    template = "products/edit-product.html"
+    context = {
+        "form": form,
+        "product": product,
+    }
+    return render(request, template, context)
