ff0bc5f29c08da397aecbf63601b589f1444c5e8
marlene32100 <katia.debon@hotmail.it>
2021-02-09 09:07:47 +0000

    Added links for superuser in products template and product detail to edit or delete item

--- products/views.py
+++ products/views.py
@@ -97,9 +97,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Great! Product added.')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product. Check your form.')
     else:
@@ -133,3 +133,12 @@ def edit_product(request, product_id):
         "product": product,
     }
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+
+    product.delete()
+    messages.success(request, f'{product.name} correctly deleted')
+
+    return redirect(reverse('products'))
