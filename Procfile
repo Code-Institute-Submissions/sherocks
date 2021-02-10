f03775e98ad17a84b00230283c565feb40b2ac05
marlene32100 <katia.debon@hotmail.it>
2021-02-10 12:14:35 +0000

    installed gunicorn and added Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn sherocks.wsgi:application
--- she_rocks/settings.py
+++ she_rocks/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'w(=z=^gjg@7yril*k9fk$y(_a&=)h-t7%u(yl(oi&s75wuynpl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["sherocks.herokuapp.com", "localhost"]
 
 
 # Application definition
@@ -120,12 +120,17 @@ WSGI_APPLICATION = 'she_rocks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if "DATABASE_URL" in os.environ:
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
