04ec05ddf8abacac2d01b688e249905de739a68d
marlene32100 <katia.debon@hotmail.it>
2021-02-21 14:51:00 +0000

    Added missing hidden value -size- in shopping bag form

--- checkout/models.py
+++ checkout/models.py
@@ -51,6 +51,7 @@ class Order(models.Model):
         self.order_total = self.lineitems.aggregate(
             Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            sdp = settings.STANDARD_DELIVERY_PERCENTAGE
             self.delivery_cost = (
                 self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         else:
@@ -81,7 +82,7 @@ class OrderLineItem(models.Model):
         max_length=2, null=True, blank=True)
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(
-        max_digits=6, decimal_places=2, null=False,
+        max_digits=10, decimal_places=2, null=False,
         blank=False, editable=False)
 
     def save(self, *args, **kwargs):
